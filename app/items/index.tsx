6173967b2e7483a522deeca0d0d2d85070a72122
Jahid Hasan <jahidhasanimonn@gmail.com>
2025-02-07 02:35:33 +0600

    Refactor items/index to reduce duplication

--- app/items/index.tsx
+++ app/items/index.tsx
@@ -5,16 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Items() {
   return (
     <View style={styles.container}>
-      <Link href={routes.items.new} style={styles.link}>
-        Create new Item
-      </Link>
-      <Text style={styles.customText}>Hello from Expenses!!</Text>
-      <Link href={routes.items.detail(1)} style={styles.link}>
-        Go to Item 1
-      </Link>
-      <Link href={routes.items.detail(2)} style={styles.link}>
-        Go to Item 2
-      </Link>
+      <Text style={styles.customText}>Hello from Items List!!</Text>
     </View>
   );
 }
@@ -29,14 +20,5 @@ const styles = StyleSheet.create({
   customText: {
     fontSize: 24,
     fontWeight: "bold",
-  },
-  link: {
-    color: "blue",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    backgroundColor: "#f0f0f0",
-    marginBottom: 5,
-    marginTop: 5,
-    fontSize: 16,
   },
 });
